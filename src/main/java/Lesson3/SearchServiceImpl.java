b8a7a64c192595042f16e8859887a684d226cb90
golubevc <golubevalexeyd@gmail.com>
2024-03-19 22:21:37 +0300

    Обновления

--- src/main/java/Lesson3/SearchServiceImpl.java
+++ src/main/java/Lesson3/SearchServiceImpl.java
@@ -8,10 +8,10 @@ public class SearchServiceImpl implements SearchService {
         if (animal == null) {
             throw new InvalidAnimalException("на вход пришло некорректный объект животного" + today);
         } else if (animal.getBirthDate() == null) {
-            throw new InvalidAnimalException(Animal.getName());
-        } else if (animal.getBirthDate().isLeapYear()) System.out.println(Animal.getName() + " был рожден в високосный год ");
+            throw new InvalidAnimalException(animal.getName());
+        } else if (animal.getBirthDate().isLeapYear()) System.out.println(animal.getName() + " был рожден в високосный год ");
         else {
-            System.out.println(Animal.getName() + " был рожден в невисокосный год ");
+            System.out.println(animal.getName() + " был рожден в невисокосный год ");
         }
     }
 }
