88d32321c1b89116eeec95e9031cf47e3a5f12cd
golubeva <golubeva@mtsbank.ru>
2024-03-19 20:48:25 +0300

    Подготовил SearchService и SearchServiceImpl.java, правда они не работают. Дописал их, все равно не работают))))

--- src/main/java/Lesson3/Main.java
+++ src/main/java/Lesson3/Main.java
@@ -4,20 +4,16 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) throws InvalidAnimalException {
-        try {
-            CreateAnimalService createAnimalService = new CreateAnimalServiceImpl();
+/*            CreateAnimalService createAnimalService = new CreateAnimalServiceImpl();
             createAnimalService.createAnimal();
 
             CreateAnimalServiceImpl createAnimalServiceImpl = new CreateAnimalServiceImpl();
             createAnimalServiceImpl.createAnimal(5);
-            createAnimalServiceImpl.createAnimal();
+            createAnimalServiceImpl.createAnimal();*/
 
             SearchServiceImpl searchServiceimpl = new SearchServiceImpl();
             searchServiceimpl.checkLeapYearAnimal(Animal);
 
-        }catch (InvalidAnimalBirthDateException e) {
-            System.out.println("выбросили ошибку");
-        }
 
         }
     }
